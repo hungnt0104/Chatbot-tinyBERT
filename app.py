142adf0b3ce02419527a0cf4cb3e150a4e9430af
hungnt0104 <nguyenthanhhung.thcneu@gmail.com>
2024-04-25 11:51:49 +0700

    adding

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, flash
+from flask_cors import CORS
 from main import get_response
 
 app = Flask(__name__)
+CORS(app)
 
 messages = []
 questions = []
@@ -15,12 +17,18 @@ def index():
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
-    user_message = request.form.get('user_message')
+    data = request.json
+    # print(request.json['user_message'])
+    # print(request.form)
+    # user_message = request.form.get('user_message')
+    user_message = request.json['user_message']
+    print(user_message)
     if user_message:
         label = get_response(user_message)
         messages.append(user_message)
         messages.append(label)
         messages_with_index = list(enumerate(messages))
-        return render_template('index.html', messages_with_index=messages_with_index)
+        # return render_template('index.html', messages_with_index=messages_with_index)
+        return label
 if __name__ == '__main__':
     app.run(debug=True)
--- test.js
+++ test.js
@@ -2,7 +2,8 @@ const axios = require('axios');
 
 async function getPythonPrediction(message) {
   try {
-    const response = await axios.post('http://127.0.0.1:5000/send_message', { user_message: message });
+    const response = await axios.post('http://127.0.0.1:5000/send_message', { user_message: "What is the process for submitting my idea to the school?" });
+    // console.log(response)
     return response.data;
   } catch (error) {
     console.error('Error:', error.message);
